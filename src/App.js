190ea58da6f0bd608c2acd4569b7d5b92e38cd08
gobanoff <a.kliosovas@yahoo.com>
2023-08-31 20:50:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState();
+  const [number, setNumber] = useState(true);
+  const [symbol, setSymbol] = useState(true);
+
+  const [data, setData] = useState([]);
+
+  const lowLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numb = "0123456789";
+  const symb = "!@#$%^&*()-_+=<>?";
+
+  useEffect(() => {
+    const savedPasswords =
+      JSON.parse(localStorage.getItem("savedPasswords")) || [];
+    setData(savedPasswords);
+  }, []);
+
+  const genPassword = (length, number, symbol) => {
+    let valChar = lowLetters + upLetters;
+    if (number) {
+      valChar += numb;
+    }
+    if (symbol) {
+      valChar += symb;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const ranIndex = Math.floor(Math.random() * valChar.length);
+      password += valChar[ranIndex];
+    }
+
+    return password;
+  };
+
+  const handPassword = () => {
+    const newPassword = genPassword(length, number, symbol);
+    const updatedPasswords = [...data, newPassword];
+    setData(updatedPasswords);
+    localStorage.setItem("savedPasswords", JSON.stringify(updatedPasswords));
+    console.log(newPassword);
+    setPassword(newPassword);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Need a password? Try the 1Password Strong <br />
+        Password Generator.
+      </h1>
+
+      <input className="control" type="text" value={password} readOnly />
+      <input
+        className="nr"
+        type="number"
+        min="6"
+        max="20"
+        value={length}
+        onChange={(e) => setLength(parseInt(e.target.value))}
+      />
+      <label className="pass"> Password length</label>
+      <input
+        className="numb"
+        type="checkbox"
+        checked={number}
+        onChange={() => setNumber(!number)}
+      />
+      <label> Numbers</label>
+      <input
+        className="symb"
+        type="checkbox"
+        checked={symbol}
+        onChange={() => setSymbol(!symbol)}
+      />
+      <label> Symbols</label>
+      <button className="gen" onClick={handPassword}>
+        Generate Password
+      </button>
+      <p className="rez">
+        {data.map((password, index) => (
+          <p key={index}>{password}</p>
+        ))}
+      </p>
     </div>
   );
 }
